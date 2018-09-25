a9eae5114d8edeb74f3520142d7c09092aa93367
Victor Magalhães <victorhfmag@gmail.com>
2018-09-25 10:33:00 -0300

    feat: Make middleware options typed

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -6,6 +6,9 @@ const webPreset = require("@cubos/neutrino-preset-web");
  * @param {webPreset.INeutrino} neutrino
  */
 module.exports = (neutrino) => {
+  /**
+   * @template import("@cubos/neutrino-preset-web").IPresetOptions
+   */
   neutrino.use(webPreset.middleware, {
     react: {
       html: {
