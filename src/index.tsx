e6ed16e368ba7aabcc34176a458181297ad3a142
Lucas Landim <lucas.landim@cubos.io>
2018-10-08 12:34:51 +0000

    Revert "Fix: Unnecessary files removed"
    This reverts commit b98f1224918df17157b4573da854352e1bcdeb1b

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,17 @@ import { Provider } from "mobx-react";
 import { Router } from "react-router";
 import { RouterStore, syncHistoryWithStore } from "mobx-react-router";
 import createBrowserHistory from "history/createBrowserHistory";
+import { AppContainer } from "react-hot-loader";
+
+import { CounterStore } from "./stores";
+
 import App from "./containers/App";
 
 const routerStore = new RouterStore();
+const counterStore = new CounterStore();
 const rootStore = {
 	routerStore,
+	counterStore,
 };
 
 const browserHistory = createBrowserHistory();
@@ -18,11 +24,20 @@ if (process.env.NODE_ENV !== "production") {
 	console.log("Staging Mode");
 }
 
-ReactDOM.render(
-	<Provider {...rootStore}>
-		<Router history={history}>
-			<App />
-		</Router>
-	</Provider>,
-	document.getElementById("root"),
-);
+const render = () =>
+	ReactDOM.render(
+		<AppContainer>
+			<Provider {...rootStore}>
+				<Router history={history}>
+					<App />
+				</Router>
+			</Provider>
+		</AppContainer>,
+		document.getElementById("root"),
+	);
+
+if (module.hot) {
+	module.hot.accept(undefined, render);
+}
+
+render();
